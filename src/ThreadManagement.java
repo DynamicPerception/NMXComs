f7b4baa5ba176629145e5ff73ddefb04aeb16c9a
Michael Ploof <michael@dynamicperception.com>
2016-01-19 18:06:08 -0500

    Initial commit

---
+++ src/ThreadManagement.java
@@ -0,0 +1,10 @@
+class ThreadManagement {
+	
+	public static void message(String message) {
+	    String threadName =
+	        Thread.currentThread().getName();
+	    System.out.format("%s: %s%n",
+	                      threadName,
+	                      message);
+	}
+}
